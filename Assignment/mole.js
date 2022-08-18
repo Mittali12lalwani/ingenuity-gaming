23a1114db024def7850aa2e264111483aaa56242
Mittali12lalwani <lalwanimitali2001@gmail.com>
2022-08-18 15:45:33 +0530

    update

---
+++ Assignment/mole.js
@@ -0,0 +1,54 @@
+var h1 = document.querySelector("h1");
+var startermole = document.querySelector("#mole");
+var starterdirt = document.querySelector("#mud");
+var startbtn = document.querySelector("#start");
+var points = document.querySelectorAll(".points");
+var dirts = document.querySelectorAll(".dirt");
+var mole = document.querySelectorAll(".mole");
+var current=document.getElementById('currentPoint')
+
+function start(){
+    h1.style.visibility = "hidden";
+    startermole.style.visibility = "hidden";
+    starterdirt.style.visibility = "hidden";
+    startbtn.style.visibility = "hidden";
+    
+    for (var i=0; i<2;i++) points[i].style.visibility = "visible";
+
+    for (var i=0; i<6;i++) dirts[i].style.visibility = "visible"; 
+    for(var i=0;i<6;i++){
+        dirts[i].style.visibility="visible";
+     }
+     var inc=0;
+     setInterval(moleVisible,1000);
+      
+     
+
+     
+    }
+   
+         function randomNumber(min,max){
+            return Math.floor(Math.random()*(max-min+1)+min);
+         }
+         function moleVisible(){
+            // var inc=0;
+            var b=randomNumber(1,6);
+            mole[b-1].style.visibility="visible";
+            mole[b-1].onclick = updatePoints;
+
+
+    setTimeout(()=>{for(var i=0;i<6;i++){
+        // moles[i].onclick=incrementPoints;
+        mole[i].style.visibility="hidden";
+    }},900)
+
+ }
+ var inc=0;
+ function updatePoints(){
+   
+    inc++;
+    currentPoint.textContent=inc;
+    console.log(inc);
+ }
+
+
