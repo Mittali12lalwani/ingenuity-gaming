71ad55a891cde502015ce7c39e292bf0111c6d43
Mittali12lalwani <lalwanimitali2001@gmail.com>
2022-09-05 18:24:37 +0530

    done

--- dist/main3.js
+++ dist/main3.js
@@ -1,1 +1,20 @@
 "use strict";
+function varargs1(...args) {
+    let sum1 = 0;
+    for (let i = 0; i < args.length; i++) {
+        sum1 += args[i];
+    }
+    return sum1;
+}
+let value = prompt("Enter length of arguments");
+let value1 = parseInt(value);
+let y = [];
+for (let i = 0; i < value1; i++) {
+    let mynumber = prompt("Enter number");
+    let num = parseInt(mynumber);
+    y.push(num);
+}
+document.body.style.textAlign = "center";
+document.body.style.color = "white";
+document.body.style.fontSize = "24px";
+document.write("My sum is: " + varargs1(...y));
---
+++ src/main3.ts
@@ -0,0 +1,30 @@
+// let number2=prompt(`Enter a number`);
+
+// let a:number=parseInt(number2);
+// var arr2[];
+// arr2.push(number2);
+
+
+function varargs1(...args:number[]){
+    let sum1=0;
+
+    for(let i=0;i<args.length;i++){
+        sum1+=args[i];
+    }
+    return sum1;
+}
+// varargs1(1,2,3);
+
+let value:any=prompt("Enter length of arguments");
+let value1=parseInt(value);
+let y:number[]=[];
+for(let i=0;i<value1;i++){
+    let mynumber:any=prompt("Enter number");
+    let num=parseInt(mynumber);
+    y.push(num);
+}
+document.body.style.textAlign="center";
+document.body.style.color="white";
+document.body.style.fontSize="24px";
+
+document.write("My sum is: "+varargs1(...y));
